e15cc211db05b4acfcdd94e3c7ca59bc51b0f5f7
voktopp <103954164+voktopp@users.noreply.github.com>
2022-04-18 19:52:29 +0300

    Create xcvfd.exs

---
+++ xcvfd.exs
@@ -0,0 +1,8 @@
+defmodule Hangman.GameTest do
+  use ExUnit.Case
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+    game = Game.new_game()
+    assert(game.turns_left == 7)
+    assert(game.game_state == :initializing)
